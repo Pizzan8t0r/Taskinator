f36e833585fcabe1904d25fd8a53465efe5d5099
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-01 16:06:14 -0700

    created the task app's script and included some variables

---
+++ script.js
@@ -0,0 +1,9 @@
+var taskIdCounter = 0;
+
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
+var pageContentEl = document.querySelector("#page-content");
+
+// create array to hold tasks for saving
